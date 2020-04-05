0f078b6fbc11f23bbc56ee8019f95bf0ef45bb11
Shea Faigan <stfaigan@gmail.com>
2020-04-05 13:36:46 -0700

    Implemented free block vector in InitLLFS; goal 1 complete (I think)

--- apps/test02.c
+++ apps/test02.c
@@ -13,19 +13,20 @@ void test_InitLLFS() {
   FILE* disk = fopen(vdisk_path, "r+");
   char* buffer = (char*)malloc(BLOCK_SIZE);
   read_block(disk, 0, buffer);
-  fclose(disk);
 
   int magic_number = MAGIC_NUMBER;
   int num_blocks = NUM_BLOCKS;
   int num_inodes = NUM_INODES;
 
-  int magic_number_present = memcmp(buffer, &magic_number, sizeof(magic_number) != 0);
-  int num_blocks_present = memcmp(buffer + sizeof(magic_number), &num_blocks, sizeof(num_blocks) != 0);
-  int num_inodes_present = memcmp(buffer + sizeof(magic_number) + sizeof(num_blocks), &num_inodes, sizeof(num_inodes) != 0);
+  int magic_number_present = memcmp(buffer, &magic_number, sizeof(magic_number));
+  int num_blocks_present = memcmp(buffer + sizeof(magic_number), &num_blocks, sizeof(num_blocks));
+  int num_inodes_present = memcmp(buffer + sizeof(magic_number) + sizeof(num_blocks), &num_inodes, sizeof(num_inodes));
 
-  free(buffer);
+  read_block(disk, 1, buffer);
+  int bitmap = 0x03FF;
+  int bitmap_present = memcmp(buffer, &bitmap, sizeof(bitmap));
 
-  if (magic_number_present == 0 && num_blocks_present == 0 && num_blocks_present == 0) {
+  if (magic_number_present == 0 && num_blocks_present == 0 && num_blocks_present == 0 && bitmap_present == 0) {
     printf("PASS\n");
   } else {
     printf("FAILURE:\n");
@@ -35,7 +36,11 @@ void test_InitLLFS() {
       printf("Number of blocks not contained in second 4 bytes of superblock.\n");
     if (num_inodes_present != 0)
       printf("Number of i-nodes not contained in third 4 bytes of superblock.\n");
+    if (bitmap_present != 0)
+      printf("Free block vector not initialized correctly.\n");
   }
+  free(buffer);
+  fclose(disk);
 }
 
 void main() {
--- io/File.c
+++ io/File.c
@@ -14,37 +14,72 @@ typedef struct Superblock {
   int num_inodes;
 } Superblock;
 
+//https://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit
+void set_block(unsigned char buffer[], int block_num)
+{
+  int index = block_num / 8;
+  int bit_index = block_num % 8;
+
+//  printf("SET\n");
+//  printf("Index: %d\n", index);
+//  printf("Bit Index: %d\n", bit_index);
+  buffer[index] |= 1UL << bit_index;
+}
+
+void unset_block(unsigned char buffer[], int block_num)
+{
+  int index = block_num / 8;
+  int bit_index = block_num % 8;
+
+//  printf("UNSET\n");
+//  printf("Index: %d\n", index);
+//  printf("Bit Index: %d\n", bit_index);
+  buffer[index] &= ~(1UL << bit_index);
+}
+
 // Create and mount the disk (I think)
 void InitLLFS() {
-  // Create a disk (filled with 0's)
+  /* CREATE DISK */
+  // Fill with 0's
   FILE* disk = fopen(vdisk_path, "w+");
   char* zeros = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
   fwrite(zeros, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
   free(zeros);
   fclose(disk);
 
-  // Initialize Block 0 (Superblock)
+  /* FORMAT DISK */
+  // Open disk
   disk = fopen(vdisk_path, "w");
   if (!disk) {
     fprintf(stderr, "ERROR: Could not open %s.\n", vdisk_path);
     exit(EXIT_FAILURE);
   }
-  char* buffer = (char*)calloc(BLOCK_SIZE, 1);
 
+  // Initialize buffer
+  char* buffer = (char*)calloc(BLOCK_SIZE, 1);
   if (!buffer) {
     fprintf(stderr, "ERROR: Failed to allocate buffer.\n");
     exit(EXIT_FAILURE);
   }
 
+  // Initialize Superblock
   Superblock* sb = (Superblock*)malloc(sizeof(Superblock));
-
   sb->magic_number = MAGIC_NUMBER;
   sb->num_blocks = NUM_BLOCKS;
   sb->num_inodes = NUM_INODES;
 
+  // Write the superblock to the disk
   memcpy(buffer, sb, sizeof(Superblock));
-
   write_block(disk, 0, buffer);
+
+  // Initialize the free block bitmap
+  memset(buffer, 0, BLOCK_SIZE);
+  for (int i = 0; i < 10; i++)
+    set_block(buffer, i);
+
+  // Write free block bitmap to the disk
+  write_block(disk, 1, buffer);
+
   free(buffer);
   fclose(disk);
 }
